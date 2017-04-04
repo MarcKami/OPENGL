bca361c253a05e32d28e54aa4d46f52179d4e097
MarcKami <Marc.kami1991@gmail.com>
2017-04-04 11:40:57 +0200

    Doing things right
    rename and linking the project correctly

--- Practica1/src/main.cpp
+++ Practica1/src/main.cpp
@@ -4,15 +4,31 @@
 //GLFW
 #include <GLFW\glfw3.h>
 #include <iostream>
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
 #include "..\Shader.h"
+#include <SOIL.h>
 
+using namespace glm;
 using namespace std;
 const GLint WIDTH = 800, HEIGHT = 600;
 bool WIDEFRAME = false;
-bool paintQuad=false;
+bool paintQuad = false;
+bool fade1 = false;
+float mixStuff;
+float rotacionX,rotacionY = 0.0f;
+float gradosRot = 0;
+float aumentoRot;
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+bool aumentarRotRight, aumentarRotLeft,aumentarUp,aumentarDown;
 
-void DrawVao(GLuint programID,GLuint VAO) {
+
+bool camUp, camDown, camLeft, camRight;
+vec3 camPosVec, camDirVec, camRightVec;
+float camSpeed = 0.15f;
+
+void DrawVao(GLuint programID, GLuint VAO) {
 	//establecer el shader
 	glUseProgram(programID);
 
@@ -28,11 +44,23 @@ void DrawVao(GLuint programID,GLuint VAO) {
 	}
 
 }
-int main() {
+
+mat4 GenerateModelMatrix(vec3 aTranslation, vec3 aRotation, vec3 CubesPosition, float aRot) {
+	mat4 temp;
+	temp = translate(temp, aTranslation);
+	temp = translate(temp, CubesPosition);
+	temp = rotate(temp, radians(aRot), aRotation);
+
+	return temp;
+}
+
+
+void main() {
+	mixStuff = 0.0f;
 	//initGLFW
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
-//TODO
+	//TODO
 
 	//set GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -42,7 +70,7 @@ int main() {
 
 
 	//create a window
-//TODO
+	//TODO
 	GLFWwindow* window;
 
 
@@ -56,38 +84,41 @@ int main() {
 	glfwMakeContextCurrent(window);
 
 	//set GLEW and inicializate
-//TODO
+	//TODO
 
 	glewExperimental = GL_TRUE;
 	if (GLEW_OK != glewInit()) {
 		std::cout << "Error al inicializar glew" << std::endl;
 		glfwTerminate();
-		return NULL;
+		exit(EXIT_FAILURE);
 	}
 	int screenWithd, screenHeight;
 	glfwGetFramebufferSize(window, &screenWithd, &screenHeight);
 	//set function when callback
-//TODO
-		glfwSetKeyCallback(window, key_callback);
+	//TODO
+	glfwSetKeyCallback(window, key_callback);
 
 	//set windows and viewport
-//TODO
-		glViewport(0, 0, screenWithd, screenHeight);
-		glClear(GL_COLOR_BUFFER_BIT);
+	//TODO
+	glViewport(0, 0, screenWithd, screenHeight);
+	glClear(GL_COLOR_BUFFER_BIT);
 
 	//fondo
-		glClearColor(0.0, 0.0, 1.0, 1.0);
+	glClearColor(0.0, 0.0, 1.0, 1.0);
 
 
-//TODO
+	//TODO
 
 
 	//cargamos los shader
 
-		Shader shader = Shader("./src/SimpleVertexShader.vertexshader", "./src/SimpleFragmentShader.fragmentshader");
-		GLuint programID = shader.Program;
+	//Shader shader = Shader("./src/SimpleVertexShader.vertexshader", "./src/SimpleFragmentShader.fragmentshader");
 
-		//GLuint programID = LoadShaders("./src/SimpleVertexShader.vertexshader", "./src/SimpleFragmentShader.fragmentshader");
+	Shader shader = Shader("./src/textureVertex3d.vertexshader", "./src/textureFragment3d.fragmentshader");
+
+	//GLuint programID = shader.Program;
+
+	//GLuint programID = LoadShaders("./src/SimpleVertexShader.vertexshader", "./src/SimpleFragmentShader.fragmentshader");
 
 	// Definir el buffer de vertices
 	//Reserva de memoria
@@ -100,17 +131,83 @@ int main() {
 	//};
 
 	//VBO
-		GLfloat VertexBufferObject[] = {
-			0.5f,  0.5f, 0.0f,  // Top Right
-			0.5f, -0.5f, 0.0f,  // Bottom Right
-			-0.5f, -0.5f, 0.0f,  // Bottom Left
-			-0.5f,  0.5f, 0.0f   // Top Left 
-		};
+	//GLfloat VertexBufferObject[] = {
+	//	0.5f,  0.5f, 0.0f,  // Top Right
+	//	0.5f, -0.5f, 0.0f,  // Bottom Right
+	//	-0.5f, -0.5f, 0.0f,  // Bottom Left
+	//	-0.5f,  0.5f, 0.0f   // Top Left 
+	//};
+
+	//GLfloat VertexBufferObject[] = {
+	//	// Positions          // Colors           // Texture Coords
+	//	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+	//	0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+	//	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+	//	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left 
+	//};
+
+	GLfloat VertexBufferObject[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f , -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	vec3 CubesPositionBuffer[] = {
+		vec3(0.0f ,  0.0f,  0.0f),
+		vec3(2.0f ,  5.0f, -15.0f),
+		vec3(-1.5f, -2.2f, -2.5f),
+		vec3(-3.8f, -2.0f, -12.3f),
+		vec3(2.4f , -0.4f, -3.5f),
+		vec3(-1.7f,  3.0f, -7.5f),
+		vec3(1.3f , -2.0f, -2.5f),
+		vec3(1.5f ,  2.0f, -2.5f),
+		vec3(1.5f ,  0.2f, -1.5f),
+		vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 
 	//Numbuffer = cantidad de buffers a generar;
 
-		//Borrado
-		//glDeleteBuffers(GLsizei n, const GLuint* ids);
+	//Borrado
+	//glDeleteBuffers(GLsizei n, const GLuint* ids);
 
 	//-VBO
 
@@ -119,26 +216,24 @@ int main() {
 	//GLuint IndexBufferObject[]{
 	//	0,1,2,
 	//	1,3,0 };
-		GLuint IndexBufferObject[]{
-		2,0,3,
-		2,1,0
 
-		};
+	//GLuint IndexBufferObject[]{
+	//	2,0,3,
+	//	2,1,0
+	//};
+
+
+
+
 	//-EBO
-
-	int numBuffer = 1;
-
-
-
-
 
 
 	// Definir el EBO
 
 	// Crear los VBO, VAO y EBO
-	
-	GLuint VAO, EBO, VBO;
-	glGenVertexArrays(1,&VAO);
+
+	GLuint VAO, /*EBO,*/ VBO;
+	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO); {
 
 		glGenBuffers(1, &VBO);
@@ -148,17 +243,23 @@ int main() {
 		glBufferData(GL_ARRAY_BUFFER, sizeof(VertexBufferObject), VertexBufferObject, GL_DYNAMIC_DRAW);
 
 
-		glGenBuffers(1, &EBO);
+	/*	glGenBuffers(1, &EBO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(IndexBufferObject), IndexBufferObject, GL_DYNAMIC_DRAW);
-		
+		*/
 
 		//Propiedades
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+		
+		//glVertexAttribPointer(1, 0, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+		//glEnableVertexAttribArray(1);
+
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+		glEnableVertexAttribArray(2);
 
 		//LIMPIA LOS BUFFERS DE VERTICES
-		glEnableVertexAttribArray(0);
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -171,49 +272,224 @@ int main() {
 
 
 	//Establecer el objeto
-		//Declarar el VBO y el EBO
+	//Declarar el VBO y el EBO
 
-		//Enlazar el buffer con openGL
+	//Enlazar el buffer con openGL
 
-		//Establecer las propiedades de los vertices
+	//Establecer las propiedades de los vertices
 
-		//liberar el buffer
-	
+	//liberar el buffer
+
 	//liberar el buffer de vertices
 
+
+	//GLint loc = glGetUniformLocation(shader.Program, "time");
+
+
+	GLuint texture1, texture2;
+
+	//GLint matProjID,matViewID,matModelID;
+	GLint matrizDefID;
+
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int widthTex, heightTex;
+	//widthTex = heightTex = 512;
+	unsigned char* image = SOIL_load_image("./src/texture.png", &widthTex, &heightTex, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthTex, heightTex, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	SOIL_free_image_data(image);
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int widthTex2, heightTex2;
+
+	unsigned char* image2 = SOIL_load_image("./src/texture2.png", &widthTex2, &heightTex2, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthTex2, heightTex2, 0, GL_RGB, GL_UNSIGNED_BYTE, image2);
+	SOIL_free_image_data(image2);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	aumentoRot = 0.5f;
+	
+	glEnable(GL_DEPTH_TEST);
 	//bucle de dibujado
 
-	GLint loc = glGetUniformLocation(shader.Program,"time");
+	camPosVec = vec3(0.f, 0.f, -3.f);
+	camDirVec = (vec3(0.f, 0.f, 0.f) - camPosVec) / glm::length((vec3(0.f, 0.f, 0.f) - camPosVec));
+	camRightVec = glm::cross(camDirVec, vec3(0,1,0) / glm::length(glm::cross(camDirVec, vec3(0, 1, 0))));
 
 	while (!glfwWindowShouldClose(window))
 	{
+		//mat4 finalMatrix; //Modelo
+		mat4 cam; //Vista
+		mat4 proj; //Proyeccion
+
+
+		//mat4 proj = perspective(60, screenWithd/screenHeight, 0.1f, 100);
+
+
+		if (gradosRot > 360 || gradosRot < -360) {
+			gradosRot = 0;
+		}
+
+
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 
+		/*if (loc != -1)
+		{
 		glUniform1f(loc, glfwGetTime());
+		}*/
 
 		glfwPollEvents();
+
+		//matModelID = glGetUniformLocation(shader.Program, "matrizModel");
+		//matProjID = glGetUniformLocation(shader.Program, "matrizProj");
+		//matViewID = glGetUniformLocation(shader.Program, "matrizView");
+		matrizDefID = glGetUniformLocation(shader.Program, "matrizDefinitiva");
+
 		//Establecer el color de fondo
-		glClear(GL_COLOR_BUFFER_BIT);
+		//glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+		GLint locTex = glGetUniformLocation(shader.Program, "ourTexture");
+		GLint locTex2 = glGetUniformLocation(shader.Program, "ourTexture2");
+		GLint mixID = glGetUniformLocation(shader.Program, "mixStuff");
+		shader.USE();
 		
+		glUniform1f(mixID, mixStuff);
+
+
 		//glClearColor(0, 1, 0, 0);
 
-		DrawVao(programID,VAO);
-	//	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//DrawVao(shader.Program,VAO);
+		//	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glDrawElements(GL_POLYGON, 6, GL_UNSIGNED_INT, 0);
-		glBindVertexArray(0);
-		shader.USE();
+
+
+		//glBindVertexArray(0);
+
+		//glBindTexture(GL_TEXTURE_2D, texture1);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glUniform1i(locTex,0);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		glUniform1i(locTex2, 1);
+		
+		glBindVertexArray(VAO);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		
+		
 		//pintar con lineas
 		//pintar con triangulos
 
+		if (aumentarRotLeft) {
+			rotacionY-=aumentoRot;
+		}
+		else if (aumentarRotRight) {
+			rotacionY+= aumentoRot;
+		}
+
+		if (aumentarUp) {
+			rotacionX -= aumentoRot;
+		}
+		else if (aumentarDown) {
+			rotacionX += aumentoRot;
+		}
+
+		if (fade1) {
+			if (mixStuff >= 0&&mixStuff<1) {
+				mixStuff += 0.01f;
+			}
+		}
+		else {
+			if (mixStuff>0.01f) {
+				mixStuff -= 0.01f;
+			}
+		}
+
+		//Cam movement
+		//DoMovement(window);
+
+		if (camUp) {
+			camPosVec += camDirVec * camSpeed;
+		}
+		else if (camDown) {
+			camPosVec -= camDirVec * camSpeed;
+
+		}
+
+		if (camRight) {
+			camPosVec += camRightVec * camSpeed;
+		}
+		else if (camLeft) {
+			camPosVec -= camRightVec * camSpeed;
+		}
+
+		//matriz = translate(matriz, vec3(0.5f, 0.5f, 0));
+
+
+		//matriz = scale(matriz, vec3(0.5f, -0.5f, 0.0f));
+
+		float FOV = 45.0f;
+
+		proj = perspective(FOV, (float)(800/600), 0.1f, 100.0f);
+
+		cam = translate(cam, vec3(camPosVec.x, camPosVec.y, camPosVec.z));
+
+		//glUniformMatrix4fv(matProjID, 1, GL_FALSE, glm::value_ptr(proj));
+		//glUniformMatrix4fv(matViewID, 1, GL_FALSE, glm::value_ptr(cam));
+
+		for (int i = 0; i < 10; i++) {
+			mat4 matriz;
+			if (i == 0) {
+				matriz = translate(matriz, CubesPositionBuffer[0]);
+				matriz = rotate(matriz, radians(rotacionX), vec3(1,0,0));
+				matriz = rotate(matriz, radians(rotacionY),vec3(0,1,0));
+			}
+			else {
+				float rotot = glfwGetTime() * 100;
+				rotot = (int)rotot % 360;
+				matriz = GenerateModelMatrix(vec3(0.0f), vec3(1, 0.5f, 0), CubesPositionBuffer[i], rotot);
+			}
+			//glUniformMatrix4fv(matModelID, 1, GL_FALSE, glm::value_ptr(matriz));
+
+			mat4 matrizDefinitiva;
+
+			matrizDefinitiva = proj*cam*matriz;
+
+			glUniformMatrix4fv(matrizDefID, 1, GL_FALSE, glm::value_ptr(matrizDefinitiva));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		glBindVertexArray(0);
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
 	// liberar la memoria de los VAO, EBO y VBO
 
 	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glBindTexture(GL_TEXTURE_2D, 0);
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	//glDeleteBuffers(1, &EBO);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
@@ -229,4 +505,80 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
 		paintQuad = !paintQuad;
 	}
+
+	if (key == GLFW_KEY_UP&&action == GLFW_PRESS) {
+		aumentarUp = true;
+	}
+
+	if (key == GLFW_KEY_UP&&action == GLFW_RELEASE) {
+		aumentarUp = false;
+	}
+
+	if (key == GLFW_KEY_DOWN&&action == GLFW_PRESS) {
+		aumentarDown = true;
+	}
+
+	if (key == GLFW_KEY_DOWN&&action == GLFW_RELEASE) {
+		aumentarDown = false;
+	}
+
+	if (key == GLFW_KEY_RIGHT&&action == GLFW_PRESS) {
+		aumentarRotRight = true;
+	}
+	else if (key == GLFW_KEY_RIGHT&&action == GLFW_RELEASE) {
+		aumentarRotRight = false;
+	}
+
+	if (key == GLFW_KEY_LEFT&&action == GLFW_PRESS) {
+		aumentarRotLeft = true;
+	}
+	else if (key == GLFW_KEY_LEFT&&action == GLFW_RELEASE) {
+		aumentarRotLeft = false;
+	}
+
+	if (key == GLFW_KEY_1&&action == GLFW_PRESS) {
+		fade1 = true;
+	}
+	else if (key == GLFW_KEY_2&&action == GLFW_PRESS) {
+		fade1 = false;
+	}
+
+
+	if (key == GLFW_KEY_W&&action == GLFW_PRESS) {
+		camUp = true;
+	}
+	else if (key == GLFW_KEY_W&&action == GLFW_RELEASE) {
+		camUp = false;
+	}
+
+	if (key == GLFW_KEY_A&&action == GLFW_PRESS) {
+		camLeft = true;
+	}
+	else if (key == GLFW_KEY_A&&action == GLFW_RELEASE) {
+		camLeft = false;
+	}
+
+	if (key == GLFW_KEY_S&&action == GLFW_PRESS) {
+		camDown = true;
+	}
+	else if (key == GLFW_KEY_S&&action == GLFW_RELEASE) {
+		camDown = false;
+	}
+
+	if (key == GLFW_KEY_D&&action == GLFW_PRESS) {
+		camRight = true;
+	}
+	else if (key == GLFW_KEY_D&&action == GLFW_RELEASE) {
+		camRight = false;
+	}
+
+
 }
+
+void DoMovement(GLFWwindow* window) {
+	camUp = glfwGetKey(window, 'w');
+	camDown = glfwGetKey(window, 's');
+	camLeft = glfwGetKey(window, 'a');
+	camRight = glfwGetKey(window, 'd');
+}
+
