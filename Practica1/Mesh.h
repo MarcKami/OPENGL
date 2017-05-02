3cad9b95419f294e8a36bd9fd85785a6f6d69dc8
MarcKami <Marc.kami1991@gmail.com>
2017-05-02 15:59:54 +0200

    Try to install assimp

---
+++ Practica1/Mesh.h
@@ -0,0 +1,40 @@
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include "Shader.h"
+#include <GL\glew.h>
+#include <gtc\matrix_transform.hpp>
+#include <glm.hpp>
+#include <assimp\types.h>
+
+
+using namespace glm;
+using namespace std;
+
+struct Vertex {
+	vec3 Position;
+	vec3 Normal;
+	vec2 TexCoords;
+};
+
+struct Texture {
+	GLuint id;
+	string type;
+	aiString path;
+};
+
+class Mesh {
+public:
+	vector<Vertex> vertices;
+	vector<GLuint> indices;
+	vector<Texture> textures;
+	GLuint VAO, VBO, EBO;
+
+	Mesh(vector<Vertex> vert, vector<GLuint> ind, vector<Texture> text);
+	void setupMesh();
+	void Draw(Shader MeshShader, GLint DrawMode);
+};
+
